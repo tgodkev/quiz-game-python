070dcf85f97460ed023de7ec9933f9b486c82e42
Kevin <50965376+tgodkev@users.noreply.github.com>
2023-04-14 19:10:44 -0400

    first commit

---
+++ question_model.py
@@ -0,0 +1,5 @@
+class Question: 
+    
+    def __init__(self, question_text, question_answer):
+        self.question_text = question_text
+        self.question_answer = question_answer
---
+++ quiz_brain.py
@@ -0,0 +1,6 @@
+class Brain:
+    
+    # ask the question
+    #check if answer is correct
+    # check if we are at the end of the quiz
+        
