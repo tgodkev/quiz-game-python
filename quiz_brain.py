e3e42ae4bd73474fa175b2435a9059117121f2bf
Kevin <50965376+tgodkev@users.noreply.github.com>
2023-04-14 19:10:49 -0400

    project starting

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,3 +4,9 @@ class Brain:
     #check if answer is correct
     # check if we are at the end of the quiz
         
+        def __init__(self, q_list):
+            self.question_number = 0
+            self.question_list = q_list
+            self.score = 0
+            
+            
