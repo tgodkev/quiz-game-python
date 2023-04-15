b0c34b97f355f03f61f80e7dec7b180c861006a6
Kevin <50965376+tgodkev@users.noreply.github.com>
2023-04-14 20:51:14 -0400

    quiz finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -9,4 +10,11 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
     
-    print(question_bank)
+
+    
+quiz = QuizBrain(question_bank)  
+still_has_questions = quiz.still_has_questions()
+  
+while still_has_questions:
+    quiz.next_question()
+    still_has_questions = quiz.still_has_questions()
